90002c2ac3d9b9c66b155d2f3646ac2866e7af97
Aryan Ajmera <aryanajmera@Mjolnir-7004.local>
2026-07-29 13:15:33 +0530

    Complete Warehouse Management APIs (CRUD + role restrictions), tested in Postman

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,15 @@ const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
+const warehouseRoutes = require('./routes/warehouseRoutes');
 
 dotenv.config();
-
 const app = express();
-
 app.use(express.json());
-
 connectDB();
 
 app.use('/api/auth', authRoutes);
+app.use('/api/warehouses', warehouseRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
