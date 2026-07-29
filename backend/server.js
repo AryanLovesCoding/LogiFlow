af0e27cbc36b7bccdeebf4ee9955628d83cc2ff1
Aryan Ajmera <aryanajmera@Mjolnir-7004.local>
2026-07-29 17:28:30 +0530

    Complete Product Management APIs (CRUD, search/filter, pagination), tested in Postman

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 const warehouseRoutes = require('./routes/warehouseRoutes');
+const productRoutes = require('./routes/productRoutes');
 
 dotenv.config();
 const app = express();
@@ -11,6 +12,7 @@ connectDB();
 
 app.use('/api/auth', authRoutes);
 app.use('/api/warehouses', warehouseRoutes);
+app.use('/api/products', productRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
